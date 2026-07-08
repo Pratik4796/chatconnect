e7ccd773cb4ac3baee2adfe3f5588b72d15667b1
Pratik4796 <pratikpatil4796@gmail.com>
2026-07-08 19:05:15 +0530

    Add Socket.IO integration: implement real-time messaging with user connection handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,17 +6,16 @@ import "dotenv/config";
 import fs from "fs";
 import path from "path";
 
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
-import clerkWebhook from "./webhooks/clerk.webhook.js"
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
-const app = express();
+import { app, server } from "./lib/socket.js";
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
@@ -31,7 +30,7 @@ app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ ok: true });
+  res.status(200).json({ ok: true });
 });
 
 app.use("/api/auth", authRoutes);
@@ -39,18 +38,17 @@ app.use("/api/messages", messageRoutes);
 
 // if the public directory exists, serve the static files
 // this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
 
-if(fs.existsSync(publicDir)){
-    app.use(express.static(publicDir))
-
-    app.get("/{*any}", (req, res, next) => {
-        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
-    });
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
 }
 
-app.listen(PORT, () => {
-    connectDB();
-    console.log("Server is up and running on PORT:", PORT);
+server.listen(PORT, () => {
+  connectDB();
+  console.log("Server is up and running on PORT:", PORT);
 
-    if (process.env.NODE_ENV === "production") job.start();
+  if (process.env.NODE_ENV === "production") job.start();
 });
